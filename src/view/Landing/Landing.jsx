09fb47113205564d0f9cbecc85804a6178310cb2
EDMR <emermontes15@gmail.com>
2023-10-19 21:33:22 -0500

    Intento con vercel0.5

--- src/view/Landing/Landing.jsx
+++ src/view/Landing/Landing.jsx
@@ -8,7 +8,7 @@ export const Landing = ()=>{
       <div>
       <h1>Welcome</h1>
         <a href="/home" >
-          <img src='https://c4.wallpaperflare.com/wallpaper/414/126/559/5bd334f8d938f-wallpaper-preview.jpg' className={style.logo} alt="Country logo" />
+          <img src='../../public/world.gif' className={style.logo} alt="Country logo" />
         </a>
       </div>
       <p> This is a SPA based on countries api.<br/>
