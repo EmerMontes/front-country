7a70f4d2353ebacd199492e95601207d9795f47d
EDMR <emermontes15@gmail.com>
2023-10-20 00:23:07 -0500

    Intento con vercel1.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ActivityCreate } from './view/createActivity/ActivityCreate'
 import { AllActivity } from './view/allActivity/AllActivity'
 import { Footer } from './components/footer/Footer'
 import  {Landing}  from './view/Landing/Landing'
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Head } from './components/head/Head'
 import {Detail} from './view/Detail/Detail'
 import {Home} from './view/Home/Home'
@@ -18,12 +18,13 @@ function App() {
   <div className={isDarkMode? 'contentLigth':'contentDark'}>
     <Head/>
    <Routes >
+    <HashRouter>
     <Route path='/' element={<Landing/>}/>
     <Route path='/home' element={<Home/>}/>
     <Route path='/detail/:ID' element={<Detail/>}/>
     <Route path='/createActivity' element={<ActivityCreate/>}/>
     <Route path='/allActivity' element={<AllActivity/>}/>
-
+    </HashRouter>
    </Routes>
    <Footer/>
    
