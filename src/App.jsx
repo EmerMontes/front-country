f845348d28c59782a08f5c6e02d18d8d2038417e
EDMR <emermontes15@gmail.com>
2023-10-20 01:06:42 -0500

    Intento con vercel1.4

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { ActivityCreate } from './view/createActivity/ActivityCreate'
 import { AllActivity } from './view/allActivity/AllActivity'
 import { Footer } from './components/footer/Footer'
 import  {Landing}  from './view/Landing/Landing'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes,  } from 'react-router-dom'
 import { Head } from './components/head/Head'
 import {Detail} from './view/Detail/Detail'
 import {Home} from './view/Home/Home'
 import './styles/index.css'
 import { useSelector } from 'react-redux'
+
 
 
 function App() {
@@ -17,13 +18,19 @@ function App() {
   return (
   <div className={isDarkMode? 'contentLigth':'contentDark'}>
     <Head/>
+    <Router>
+
    <Routes >
+  
     <Route path='/' element={<Landing/>}/>
     <Route path='/home' element={<Home/>}/>
     <Route path='/detail/:ID' element={<Detail/>}/>
     <Route path='/createActivity' element={<ActivityCreate/>}/>
     <Route path='/allActivity' element={<AllActivity/>}/>
+     
    </Routes>
+    </Router>
+   
    <Footer/>
    
   </div>)
